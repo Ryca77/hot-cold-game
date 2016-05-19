558604631c7970518fbd6523a6bac30bf1b85a3f
Ryca77 <ryan.carter77@gmail.com>
2016-05-19 19:09:16 +0100

    js/app.js

--- js/app.js
+++ js/app.js
@@ -12,59 +12,67 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
-/*//starting a new game//
-function newGame() {
-}
+	//generating the initial cumputer number//
+  	var computerChoice = Math.floor(Math.random()*(100-1)+1);
+  	console.log("Computer: " + computerChoice);
 
-$('.new').click(function() {
-	newGame();
+	//taking each user input and entering in guess box//
+	$('.button').click(function() {
+		event.preventDefault();
+		var userGuess = $('.text').val();
+		var guess = parseInt(userGuess);
+		$('.text').val('');
+		$('.guessBox').append(guess + ", ");
+		$('#count').html(function(i, val)
+				{return +val+1 });
 
-});*/
+	//ensure the user is entering a usable value//
+	if (guess > 100) {
+			alert("Enter a number between 1 and 100")
+			}
+	if (guess < 1) {
+			alert("Enter a number between 1 and 100")
+			}
 
+	//running compare function//
+	console.log("User: " + guess);
+	$('#feedback').html(compare(guess,computerChoice));
 
-//taking each user input and entering in guess box//
-$('.button').click(function() {
-	event.preventDefault();
-	var userGuess = $('.text').val();
-	var guess = parseInt(userGuess);
-	$('.guessBox').append(guess + ", ");
+	//comparison of user entry and computer number//
+	function compare(choice1,choice2) {
+		while(choice1 !== choice2) {
+			if (choice1 > (choice2 + 50)) {
+				return "Ice Cold!";
+			}
+			else if (choice1 > (choice2 + 30)) {
+				return "Cold!";
+			}
+			else if (choice1 > (choice2 + 20)) {
+				return "Warm!";
+			}
+			else if (choice1 > (choice2 + 10)) {
+				return "Hot!";
+			}
+			else {
+				return "Very Hot!";
+			}
+			choice1 = choice2
+			}
+		}
+	});
 
-//ensure the user is entering a usable value//
-if (guess > 100) {
-		alert("Enter a number between 1 and 100")
-	}
-
-//generating the computer choice and running compare funtion//
-var computerChoice = Math.floor(Math.random()*(100-1)+1);
-console.log("User: " + guess);
-console.log("Computer: " + computerChoice);
-$('#feedback').html(compare(guess,computerChoice));
+	//starting a new game and generating cumputer number//
+	$('.new').click(function() {
+		var computerChoice = Math.floor(Math.random()*(100-1)+1);
+		$('.guessBox').empty();
+		$('#feedback').empty();
+		$('#count').empty();
+		console.log("Computer: " + computerChoice);
+	});
 
 });
 
-//comparison of user entry and computer number//
-function compare(choice1,choice2) {
-	while(choice1 !== choice2) {
-		if (choice1 > (choice2 + 50)) {
-			return "Ice Cold!";
-		}
-		else if (choice1 > (choice2 + 30)) {
-			return "Cold!";
-		}
-		else if (choice1 > (choice2 + 20)) {
-			return "Warm!";
-		}
-		else if (choice1 > (choice2 + 10)) {
-			return "Hot!";
-		}
-		else {
-			return "Very Hot!";
-		}
-	choice1 = choice2
-	}
-}
-
-});
+/*function newGame ()*/
 
 
 
