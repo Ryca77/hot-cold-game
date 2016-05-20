5fc3811f433ae8849129fe672dba14be6dc277f1
Ryca77 <ryan.carter77@gmail.com>
2016-05-20 18:01:29 +0100

    js/app.js

--- js/app.js
+++ js/app.js
@@ -22,15 +22,20 @@ $(document).ready(function(){
 		var userGuess = $('.text').val();
 		var guess = parseInt(userGuess);
 		$('.text').val('');
-		$('.guessBox').append(guess + ", ");
-		$('#count').html(function(i, val)
+		if (guess <= 100 && guess >= 1) {
+			$('.guessBox').append(guess + ", ");
+			$('#count').html(function(i, val)
 				{return +val+1 });
+			}
 
 	//ensure the user is entering a usable value//
 	if (guess > 100) {
 			alert("Enter a number between 1 and 100")
 			}
 	if (guess < 1) {
+			alert("Enter a number between 1 and 100")
+			}
+	if (isNaN(guess)) {
 			alert("Enter a number between 1 and 100")
 			}
 
@@ -41,7 +46,6 @@ $(document).ready(function(){
 	//comparison of user entry and computer number//
 	function compare(choice1,choice2) {
 		var difference = Math.abs(choice1 - choice2);
-		while(choice1 !== choice2) {
 			if (difference >=50) {
 				return "Ice Cold!";
 			}
@@ -54,10 +58,11 @@ $(document).ready(function(){
 			else if (difference >= 10 && difference <= 20) {
 				return "Hot!";
 			}
-			else {
+			else if (difference >= 1 && difference <= 10) {
 				return "Very Hot!";
 			}
-			choice1 = choice2
+			else if (difference === 0) {
+				return "Congratulations!" + "<br>" + "Click 'New Game' to play again.";
 			}
 		}
 	});
@@ -66,14 +71,12 @@ $(document).ready(function(){
 	$('.new').click(function() {
 		var computerChoice = Math.floor(Math.random()*(100-1)+1);
 		$('.guessBox').empty();
-		$('#feedback').empty();
-		$('#count').empty();
+		$('#feedback').text('Make Your Guess!');
+		$('#count').html('0');
 		console.log("Computer: " + computerChoice);
 	});
 
 });
-
-/*function newGame ()*/
 
 
 
